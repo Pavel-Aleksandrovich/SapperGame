358db0b0418d9269b27ee49cbf87bc66bd5a3148
Pavel Mishanin <pavelmishanin@mail.ru>
2022-03-31 10:42:34 +0700

    create auth viewController

---
+++ SapperGame/Scenes/AuthViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AuthViewController.swift
+//  SapperGame
+//
+//  Created by pavel mishanin on 31.03.2022.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
